d8e01bc25a23d0ad1a8c299fdc71cd095447de35
Turing <turingksc@gmail.com>
2020-11-26 16:24:35 +0800

    .

--- utils.py
+++ utils.py
@@ -217,7 +217,7 @@ def get_val_hter(vgg_face, spoof_classifier, real_data_list, attack_data_list, a
     criterion = torch.nn.CrossEntropyLoss().cuda()
 
     for idx, (data, labels, _) in enumerate(val_loader):
-        print(idx, data.shape())
+        print('============', idx)
         data = data.cuda().float()
         labels = labels.cuda()
         out = vgg_face(data)
