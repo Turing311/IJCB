51f820f639d23993a6d4b39435657bd1f4c114d5
Turing <turingksc@gmail.com>
2020-11-26 15:52:12 +0800

    .

--- utils.py
+++ utils.py
@@ -28,6 +28,8 @@ def get_dataset_list(dataset, type_, mode):
             db = DataLmdb("/kaggle/working/Fake/valid", db_size=28332, crop_size=128, flip=False, scale=0.00390625)
 
         for i in range(len(db)):
+            if i % 1000 == 0:
+                print('==========', i, len(db))
             face, lab_id = db[i]
             if (lab_id == 1 and type_ == 'real') or (lab_id == 0 and type_ == 'fake'):
                 data_list.append({'face': face, 'label_id': lab_id, 'class_id': 0})
